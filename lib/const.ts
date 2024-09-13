95fad36b5eed19a3d3ab43fd4dd029997812afd8
Derek Li <derekli2024@gmail.com>
2024-09-13 16:54:06 +0800

    pull

--- lib/const.ts
+++ lib/const.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const MAKDecimal = 18;
 
 // upload to R2
@@ -8,7 +7,6 @@ export const R2_BUCKET_NAME = 'inphura';
 export const R2_ACCOUNT_ID = '9ec00afc513d3b98cac2f333c9f916fd';
 export const R2_URl_HOST = 'https://pub-10c4fb0995fc4a61bdb8cdf3dd5de86f.r2.dev';
 
-=======
 export const Tokens = [
   {
     name: 'MAK',
@@ -17,4 +15,3 @@ export const Tokens = [
     logo: '/images/mak.png',
   },
 ];
->>>>>>> 662279f5a879f4c439749b521bde24087907b7df
