dade681a0dc19611550bc10fe444b502fb880925
fei guo <812773673@qq.com>
2024-09-14 20:26:28 +0800

    feat:config-file-signle

--- lib/eth.ts
+++ lib/eth.ts
@@ -1,21 +1,5 @@
+import { MainnetConfig } from './config/eth-mainnet-config';
+import { TestnetConfig } from './config/eth-testnet-config';
 import { isProduction } from './PathMap';
 
-const MainConfig = {
-  chainId: 1,
-  chainName: 'Ethereum',
-  contract: {
-    inphura: '',
-    makToken: '',
-  },
-};
-
-const TestConfig = {
-  chainId: 1337,
-  chainName: 'TestEth',
-  contract: {
-    inphura: '0x5443984fD2c4CfF01F3Ab29e3dE9084cc408265d',
-    makToken: '0x0f4801e7a6f480DF0B5330Ef38122Ea0149f269d',
-  },
-};
-
-export const EthConfig = isProduction ? MainConfig : TestConfig;
+export const EthConfig = isProduction ? MainnetConfig : TestnetConfig;
