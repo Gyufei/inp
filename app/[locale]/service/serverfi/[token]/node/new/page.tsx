24f5c9531e75c32d569fb9783d2488a26c01ddae
Derek Li <derekli2024@gmail.com>
2024-09-18 18:45:08 +0800

    register after upload

--- app/[locale]/service/serverfi/[token]/node/new/page.tsx
+++ app/[locale]/service/serverfi/[token]/node/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useAccount } from 'wagmi';
 import { useWeb3Modal } from '@web3modal/wagmi/react';
@@ -41,6 +41,9 @@ export default function RegisterForm() {
 
   const [serverNo, setServerNo] = useState('');
   const [serverImage, setServerImage] = useState<string | null>(null);
+  const [isStartUpload, setIsStartUpload] = useState(false);
+
+  const isLoading = isRegisterLoading || (serverImage && isStartUpload) || false;
 
   useEffect(() => {
     if (searchParams.get('ref')) {
@@ -51,6 +54,15 @@ export default function RegisterForm() {
   const handleImageUpload = async (imgUrl: string) => {
     setServerImage(imgUrl);
   };
+
+  const handleScheduledUpload = async () => {
+    setIsStartUpload(false);
+    goToNodes();
+  };
+
+  const goToNodes = useCallback(() => {
+    router.push('/service/serverfi/mak/nodes');
+  }, [router]);
 
   async function handleSubmit() {
     if (!address) {
@@ -84,9 +96,13 @@ export default function RegisterForm() {
 
   useEffect(() => {
     if (isRegisterSuccess) {
-      router.push('/service/serverfi/mak/nodes');
+      if (serverImage) {
+        setIsStartUpload(true);
+      } else {
+        goToNodes();
+      }
     }
-  }, [isRegisterSuccess, router]);
+  }, [isRegisterSuccess, serverImage, goToNodes]);
 
   return (
     <div className="relative overflow-hidden h-[657px]">
@@ -107,7 +123,12 @@ export default function RegisterForm() {
           >
             <div className="flex flex-col p-6">
               <div className="flex items-center justify-center mt-4">
-                <UploadImage onImageUpload={handleImageUpload} />
+                <UploadImage
+                  onImageUpload={handleImageUpload}
+                  scheduledUpload={true}
+                  isStartScheduledUpload={isStartUpload}
+                  onScheduledUpload={handleScheduledUpload}
+                />
               </div>
             </div>
             <div className="p-6 pt-0 grid gap-6">
@@ -166,7 +187,7 @@ export default function RegisterForm() {
             <div className="flex items-center p-6 pt-0">
               <button
                 onClick={handleSubmit}
-                disabled={isRegisterLoading}
+                disabled={isLoading}
                 className="font-cal bg-[#3E71FF] inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium  transition-colors disabled:pointer-events-none disabled:opacity-50 bg-primary h-10 px-4 py-2 w-full"
               >
                 {T('Register')}
@@ -175,7 +196,7 @@ export default function RegisterForm() {
           </div>
         </div>
         <Image className="mt-[56%] ml-[100px]" src="/images/metacene.png" width={303} height={35} alt="" />
-        <div className="flex items-center gap-x-5 absolute right-[10px] top-[10px]">
+        <div className="flex items-center gap-x-5 absolute -right-[100px] top-[10px]">
           <LanguageSetting />
           <ConnectBtn />
         </div>
--- components/upload-image.tsx
+++ components/upload-image.tsx
@@ -1,25 +1,59 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ImageUploader from './react-image-uploader';
 import { uploadAction } from '@/lib/api/upload-action';
 import { R2_URl_HOST } from '@/lib/const';
 import { isProduction } from '@/lib/PathMap';
 
-const UploadImage = ({onImageUpload, hideImg = false, style = {}}: {
-    onImageUpload: (url: string) => void,
-    hideImg?: boolean,
-    style?: React.CSSProperties
+const UploadImage = ({
+  onImageUpload,
+  hideImg = false,
+  style = {},
+  scheduledUpload = false,
+  isStartScheduledUpload = false,
+  onScheduledUpload,
+}: {
+  onImageUpload: (url: string) => void;
+  hideImg?: boolean;
+  style?: React.CSSProperties;
+  scheduledUpload?: boolean;
+  isStartScheduledUpload?: boolean;
+  onScheduledUpload?: () => void;
 }) => {
   const [isUploading, setIsUploading] = useState(false);
+  const [scheduledUploadData, setScheduledUploadData] = useState<{
+    imageFile: File | null;
+    imageNameKey: string;
+  }>({
+    imageFile: null,
+    imageNameKey: '',
+  });
+
+  useEffect(() => {
+    if (isStartScheduledUpload && scheduledUploadData.imageNameKey) {
+      fetchImage(scheduledUploadData.imageFile!, scheduledUploadData.imageNameKey);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isStartScheduledUpload]);
 
   async function handleFileAdded(imageFile: File) {
-    setIsUploading(true)
+    const nameKey = `${crypto.randomUUID().replace(/-/g, '')}-${imageFile.name}`;
+    if (scheduledUpload) {
+      setScheduledUploadData({ imageFile, imageNameKey: nameKey });
+      onImageUpload(`${R2_URl_HOST}/${isProduction ? 'production' : 'test'}/${nameKey}`);
+      return;
+    }
+    await fetchImage(imageFile, nameKey);
+  }
+
+  const fetchImage = async (imageFile: File, imageNameKey: string) => {
+    setIsUploading(true);
     try {
-      const response = await uploadAction({ filename: imageFile.name });
+      const response = await uploadAction(imageNameKey);
       if (!response) {
         return;
       }
-      const { url, nameKey } = await response.json();
+      const { url } = await response.json();
 
       // 2. 使用上传URL上传文件
       await fetch(url, {
@@ -30,13 +64,17 @@ const UploadImage = ({onImageUpload, hideImg = false, style = {}}: {
         body: imageFile,
       });
       console.log('Image uploaded successfully!');
-      setIsUploading(false)
-      onImageUpload(`${R2_URl_HOST}/${isProduction ? 'production' : 'test'}/${nameKey}`);
+      setIsUploading(false);
+      if (scheduledUpload) {
+        onScheduledUpload?.();
+        return;
+      }
+      onImageUpload(`${R2_URl_HOST}/${isProduction ? 'production' : 'test'}/${imageNameKey}`);
     } catch (error) {
       console.error('Error uploading image:', error);
-      setIsUploading(false)
+      setIsUploading(false);
     }
-  }
+  };
 
   function runAfterImageDelete(file: File) {
     console.log({ file });
@@ -55,7 +93,6 @@ const UploadImage = ({onImageUpload, hideImg = false, style = {}}: {
         backgroundColor: '#1014181a',
         filter: isUploading ? 'blur(2px)' : 'none',
         ...style,
-
       }}
       hideImg={isUploading ? false : hideImg}
     />
