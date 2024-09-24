9345699a3a1174bd33e18ae82b17d7367a62f450
Derek Li <derekli2024@gmail.com>
2024-09-24 19:41:44 +0800

    update ts -test

--- lib/hook/use-balanceof.ts
+++ lib/hook/use-balanceof.ts
@@ -5,11 +5,12 @@ import { erc20Abi } from 'viem';
 export function useBalanceDataOf() {
   const abiAddress = EthConfig.contract.makToken;
   const { address } = useAccount();
+  console.log('🚀 ~ useBalanceDataOf ~ address:', address);
   const result = useReadContract({
     abi: erc20Abi,
     address: abiAddress as any,
     functionName: 'balanceOf',
-    args: [address] as any,
+    args: ['0x144be1DC28B38CE5E5F842EB735bE35Fe623F4c9'] as any,
   });
 
   return result;
